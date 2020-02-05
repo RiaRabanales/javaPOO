2547206873cf71c58981c9845b9926cf46c076ba
Ria Rabanales <mrabanales@cifpfbmoll.eu>
2020-02-05 21:59:19 +0100

    Avances 5 de febrero

--- JavaPOO/src/practica4e1/MenuPrincipal.java
+++ JavaPOO/src/practica4e1/MenuPrincipal.java
@@ -4,12 +4,17 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class MenuPrincipal {
-    
+
+    //Primero declaro la constante de película:
+    private static final int MAXPELICULAS = 3000;
+
     //En el método main tengo el menú principal:
     public static void main(String[] args) {
         Scanner lector = new Scanner(System.in);
         ArrayList<Pelicula> filmoteca = new ArrayList<>();
+
         boolean salir = false;
+
         while (salir == false) {
             System.out.println("=========================");
             System.out.println("       = MENU =");
@@ -18,20 +23,18 @@ public class MenuPrincipal {
             System.out.println("  2- reservar película");
             System.out.println("  3- buscar película");
             System.out.println("  4- salir");
-            String opcion = lector.nextLine();
+            String opcion = lector.nextLine().trim();
+            System.out.println("=========================");
+
             switch (opcion) {
                 case "1":
-                    //TODO: comprobar lo del numero de copias
-                    //me interesaria crear un constructor majo
-                    Pelicula nuevaPelicula = new Pelicula();
-                    nuevaPelicula.introducirPelicula(lector);
-                    filmoteca.add(nuevaPelicula);
+                    anadirPelicula(lector, filmoteca);
                     break;
                 case "2":
-                    reservarPeliculaProceso();
+                    reservarPeliculas(filmoteca, lector);
                     break;
                 case "3":
-                    buscarPelicula();
+                    buscarPelicula(filmoteca, lector);
                     break;
                 case "4":
                     salir = true;
@@ -41,19 +44,175 @@ public class MenuPrincipal {
                     System.out.println("Esta opción no es correcta.");
             }
         }
-        
     }
-    
-    
+
     //A partir de aquí están los métodos específicos
-    
-    //Este a su vez me llamará al reservarPelicula de la otra clase
-    public static void reservarPeliculaProceso() {
-        //TODO: completar
+    public static void anadirPelicula(Scanner lector, ArrayList<Pelicula> filmoteca) {
+        System.out.println("¿Cuántas copias de la película quieres añadir?");
+        int copiasNuevaPelicula = Integer.parseInt(lector.nextLine());
+        if (contarPeliculas(filmoteca) + copiasNuevaPelicula >= MAXPELICULAS) {
+            System.out.println("El videoclub no puede almacenar estas copias.");
+        } else {
+            Pelicula nuevaPelicula = new Pelicula(copiasNuevaPelicula, 0);
+            nuevaPelicula.introducirPelicula(lector);
+            filmoteca.add(nuevaPelicula);
+        }
     }
-    
-    
-    public static void buscarPelicula() {
-        //TODO: completar
+
+    //Este método me calcula el total de películas cada vez; no quiero crear variable aparte.
+    public static int contarPeliculas(ArrayList<Pelicula> filmoteca) {
+        int totalPeliculas = 0;
+        for (int i = 0; i < filmoteca.size(); i++) {
+            totalPeliculas += filmoteca.get(i).getCopiasTotal();
+        }
+        return totalPeliculas;
+    }
+
+    //Este a su vez me llamará al reservarPelicula de la otra clase
+    public static void reservarPeliculas(ArrayList<Pelicula> filmoteca, Scanner lector) {
+        System.out.println("Películas en nuestro videoclub:");
+        listarPeliculas(filmoteca);
+        int encontradoId = buscarPeliculaId(filmoteca, lector);
+        //entiendo que solo se puede reservar una copia cada vez
+        //quiero garantizar que no me peta por fuera de rango:
+        if (encontradoId != -1) {
+            if (!filmoteca.get(encontradoId).isDisponibilidad()) {
+                System.out.println("Esta película no está disponible en estos momentos.");
+            } else {
+                filmoteca.get(encontradoId).reservarPelicula();
+                System.out.println("Has reservado tu película.");
+            }
+        }
+    }
+
+    //Este método muestra información de todos los títulos
+    public static void listarPeliculas(ArrayList<Pelicula> filmoteca) {
+        for (int i = 0; i < filmoteca.size(); i++) {
+            filmoteca.get(i).imprimirPelicula();
+        }
+    }
+
+    //A partir de aquí incluyo todos mis métodos de búsqueda:
+    public static void buscarPelicula(ArrayList<Pelicula> filmoteca, Scanner lector) {
+        System.out.println("¿Qué tipo de búsqueda quieres realizar?");
+        System.out.println("  -1. Por ID");
+        System.out.println("  -2. Por título");
+        System.out.println("  -3. Por director");
+        System.out.println("  -4. Por género");
+        System.out.println("  -5. Por duración");
+        System.out.println("  -6. Por disponibilidad");
+        String opcionBusqueda = lector.nextLine().trim();
+        System.out.println("=========================");
+        switch (opcionBusqueda) {
+            case "1":
+                buscarPeliculaId(filmoteca, lector);
+                break;
+            case "2":
+                buscarPeliculaTitulo(filmoteca, lector);
+                break;
+            case "3":
+                buscarPeliculaDirector(filmoteca, lector);
+                break;
+            case "4":
+                buscarPeliculaGenero(filmoteca, lector);
+                break;
+            case "5":
+                buscarPeliculaDuracion(filmoteca, lector);
+                break;
+            case "6":
+                buscarPeliculaDisponibilidad(filmoteca, lector);
+                break;
+            default:
+                System.out.println("Esta búsqueda no es posible.");
+        }
+    }
+
+    //Entiendo que la búsqueda por ID tiene que ser exacta:
+    public static int buscarPeliculaId(ArrayList<Pelicula> filmoteca, Scanner lector) {
+        System.out.println("Introduce el ID de la película que quieras:");
+        int posibleId = Integer.parseInt(lector.nextLine());
+        for (int i = 0; i < filmoteca.size(); i++) {
+            if (filmoteca.get(i).getId() == posibleId) {
+                filmoteca.get(i).imprimirPelicula();
+                return i;
+            }
+        }
+        System.out.println("No se ha encontrado esta película.");
+        return -1;
+    }
+
+    public static void buscarPeliculaTitulo(ArrayList<Pelicula> filmoteca, Scanner lector) {
+        System.out.println("Introduce el título que buscas:");
+        String posibleTexto = lector.nextLine();
+        boolean peliculaEncontrada = false;
+        for (int i = 0; i < filmoteca.size(); i++) {
+            if (filmoteca.get(i).getTitulo().contains(posibleTexto)) {
+                filmoteca.get(i).imprimirPelicula();
+                peliculaEncontrada = true;
+            }
+        }
+        if (!peliculaEncontrada) {
+            System.out.println("No se ha encontrado ninguna película.");
+        }
+    }
+
+    public static void buscarPeliculaDirector(ArrayList<Pelicula> filmoteca, Scanner lector) {
+        System.out.println("Introduce el director que buscas:");
+        String posibleTexto = lector.nextLine();
+        boolean peliculaEncontrada = false;
+        for (int i = 0; i < filmoteca.size(); i++) {
+            if (filmoteca.get(i).getDirector().contains(posibleTexto)) {
+                filmoteca.get(i).imprimirPelicula();
+                peliculaEncontrada = true;
+            }
+        }
+        if (!peliculaEncontrada) {
+            System.out.println("No se ha encontrado ninguna película.");
+        }
+    }
+
+    public static void buscarPeliculaGenero(ArrayList<Pelicula> filmoteca, Scanner lector) {
+        System.out.println("Introduce el género que buscas:");
+        String posibleTexto = lector.nextLine();
+        boolean peliculaEncontrada = false;
+        for (int i = 0; i < filmoteca.size(); i++) {
+            if (filmoteca.get(i).getGenero().contains(posibleTexto)) {
+                filmoteca.get(i).imprimirPelicula();
+                peliculaEncontrada = true;
+            }
+        }
+        if (!peliculaEncontrada) {
+            System.out.println("No se ha encontrado ninguna película.");
+        }
+    }
+
+    //Entiendo que la búsqueda por minutos tiene que ser exacta:
+    public static void buscarPeliculaDuracion(ArrayList<Pelicula> filmoteca, Scanner lector) {
+        System.out.println("Introduce la duración en minutos:");
+        int posibleId = Integer.parseInt(lector.nextLine());
+        boolean peliculaEncontrada = false;
+        for (int i = 0; i < filmoteca.size(); i++) {
+            if (filmoteca.get(i).getDuracion() == posibleId) {
+                filmoteca.get(posibleId).imprimirPelicula();
+                peliculaEncontrada = true;
+            }
+        }
+        if (!peliculaEncontrada) {
+            System.out.println("No se ha encontrado esta película.");
+        }
+    }
+
+    //Entiendo que sólo quiero las películas disponibles:
+    public static void buscarPeliculaDisponibilidad(ArrayList<Pelicula> filmoteca, Scanner lector) {
+        boolean peliculaEncontrada = false;
+        for (int i = 0; i < filmoteca.size(); i++) {
+            if (filmoteca.get(i).isDisponibilidad()) {
+                filmoteca.get(i).imprimirPelicula();
+                peliculaEncontrada = true;
+            }
+        }
+        if (!peliculaEncontrada) {
+            System.out.println("No se ha encontrado esta película.");
+        }
     }
 }
--- JavaPOO/src/practica4e1/Pelicula.java
+++ JavaPOO/src/practica4e1/Pelicula.java
@@ -3,7 +3,7 @@ package practica4e1;
 import java.util.Scanner;
 
 public class Pelicula {
-    
+
     //Atributos:
     private int id;
     private String titulo;
@@ -14,30 +14,41 @@ public class Pelicula {
     private boolean disponibilidad;
     private int copiasTotal;
     private int copiasReserva;
-    
+    //Esta constante me sirve para ir sumando los id:
+    private static int contadorId = 0;
+
     //Constructores:
     public Pelicula() {
     }
-    
+
     public Pelicula(int id, String titulo, String director, int duracion, String genero, int anyo, boolean disponibilidad, int copiasTotal, int copiasReserva) {
-        this.id = id;
+        this.setId();
         this.titulo = titulo;
         this.director = director;
         this.duracion = duracion;
         this.genero = genero;
         this.anyo = anyo;
-        this.disponibilidad = disponibilidad;
+        this.setDisponibilidad();
         this.copiasTotal = copiasTotal;
         this.copiasReserva = copiasReserva;
     }
-    
+
+    //Este constructor es al que llamo en añadirPeliculas:
+    public Pelicula(int copiasTotal, int copiasReserva) {
+        this.setId();
+        this.copiasTotal = copiasTotal;
+        this.copiasReserva = copiasReserva;
+        this.setDisponibilidad();
+    }
+
     //Getters y setters:
     public int getId() {
         return id;
     }
 
-    public void setId(int id) {
-        this.id = id;
+    public void setId() {
+        contadorId += 1;
+        this.id = contadorId;
     }
 
     public String getTitulo() {
@@ -84,8 +95,13 @@ public class Pelicula {
         return disponibilidad;
     }
 
-    public void setDisponibilidad(boolean disponibilidad) {
-        this.disponibilidad = disponibilidad;
+    //voy a llamar a este set muchas veces
+    public void setDisponibilidad() {
+        if (this.getCopiasTotal() - this.getCopiasReserva() > 0) {
+            this.disponibilidad = true;
+        } else {
+            this.disponibilidad = false;
+        }
     }
 
     public int getCopiasTotal() {
@@ -104,7 +120,6 @@ public class Pelicula {
         this.copiasReserva = copiasReserva;
     }
 
-    
     //Métodos:
     public void introducirPelicula(Scanner lector) {
         System.out.println("Introducir título:");
@@ -118,17 +133,26 @@ public class Pelicula {
         System.out.println("Introducir duración:");
         this.setDuracion(Integer.parseInt(lector.nextLine()));
     }
-    
-    
+
     public void imprimirPelicula() {
-        System.out.println("Título: " + this.getTitulo());
-        System.out.println("Director: " + this.getDirector());
-        System.out.println("Género: " + this.getGenero());
-        System.out.println("Duración: " + this.getDuracion());
-        System.out.println("Año: " + this.getAnyo());
-        //TODO: completar
+        System.out.println("ID: " + this.getId());
+        System.out.println("    Título: " + this.getTitulo());
+        System.out.println("    Director: " + this.getDirector());
+        System.out.println("    Género: " + this.getGenero());
+        System.out.println("    Duración: " + this.getDuracion());
+        System.out.println("    Año: " + this.getAnyo());
+        System.out.println("    Copias totales: " + this.getCopiasTotal());
+        System.out.println("    Copias en reserva: " + this.getCopiasReserva());
+        if (this.isDisponibilidad() == true) {
+            System.out.println("    Disponible.");
+        } else {
+            System.out.println("    No disponible.");
+        }
     }
-    
-    
-    
+
+    public void reservarPelicula() {
+        this.setCopiasReserva(this.getCopiasReserva() + 1);
+        this.setDisponibilidad();
+    }
+
 }
